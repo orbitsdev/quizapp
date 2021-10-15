67a4525a0f1ce7543c654df60e1d366f7834abb5
brianorbino <brianorbino@sksu.edu.ph.com>
2021-10-15 16:18:59 +0800

    add text

--- lib/home.dart
+++ lib/home.dart
@@ -6,7 +6,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:const Text("da")
+      body:const Text("googlemap")
     );
   }
 }
