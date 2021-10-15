13c36c50d7a80b6aed2b1bfad031601aa8735b57
brianorbino <brianorbino@sksu.edu.ph.com>
2021-10-15 16:06:09 +0800

    modfiy content

--- lib/home.dart
+++ lib/home.dart
@@ -6,7 +6,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:const Text("dasdasd")
+      body:const Text("da")
     );
   }
 }
